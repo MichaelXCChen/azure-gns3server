8ff972871282a6f055b3e4a0d30f02a2e33b1214
MichaelXCChen <xcmichaelchen@gmail.com>
2022-05-21 23:07:24 +1200

    added security association to nic

--- main.tf
+++ main.tf
@@ -67,12 +67,18 @@ resource "azurerm_network_interface" "nic" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
+
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+}
+
+resource "azurerm_network_interface_security_group_association" "nic-allow-ssh-and-wireguard" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.allow-ssh-and-wireguard.id
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
